a7857f790404c59378ecafa6455e9ebec3a44d3d
Evelio Tarazona <evelio@evelio.info>
2013-02-24 23:50:00 -0800

    Add some more actions filtering plus dump 'em to logcat

--- whatsnew/src/main/java/info/evelio/whatsnew/receiver/PackageReceiver.java
+++ whatsnew/src/main/java/info/evelio/whatsnew/receiver/PackageReceiver.java
@@ -3,6 +3,10 @@ package info.evelio.whatsnew.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import info.evelio.whatsnew.util.L;
+
+import java.util.Set;
 
 /**
  * @author Evelio Tarazona Cáceres <evelio@evelio.info>
@@ -11,6 +15,18 @@ public class PackageReceiver extends BroadcastReceiver {
 
   @Override
   public void onReceive(Context context, Intent intent) {
+    StringBuilder builder = new StringBuilder("Received ")
+        .append(intent);
+    Set<String> keys;
+    if (intent == null || intent.getExtras() == null || (keys = intent.getExtras().keySet()).isEmpty()) {
+      L.d("wn:PR", builder.toString());
+      return;
+    }
+    Bundle extras = intent.getExtras();
+    for (String key : keys) {
+      builder.append(" ").append(key).append(": ").append(String.valueOf(extras.get(key)));
+    }
+    L.d("wn:PR", builder.toString());
 
   }
 }
